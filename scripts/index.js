2267d3fc1ed1c308fb85352015543ebb8ed5912f
YuliaDuk <yule444ka1234@mail.ru>
2023-01-13 16:26:58 +0300

    feat: изменить адаптив формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
 let popupform = document.querySelector('.popup');
 let addbutton = document.querySelector('.button-popup');
 let closebutton = popupform.querySelector('.popup__close-icon');
+let bodyoverflow = document.querySelector('.body');
 function addForm(){
     popupform.classList.add('popup_opened');
+    bodyoverflow.setAttribute('style', 'overflow:hidden');
 }
 function closeForm(){
     popupform.classList.remove('popup_opened');
+    bodyoverflow.removeAttribute('style', 'overflow:hidden');
 }
 addbutton.addEventListener('click', addForm);
 closebutton.addEventListener('click', closeForm);
