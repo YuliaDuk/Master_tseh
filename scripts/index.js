e28084ff58b2a844ca9b33d49618cd7348543aff
YuliaDuk <yule444ka1234@mail.ru>
2023-01-15 13:48:25 +0300

    feat: добавить gform

--- scripts/index.js
+++ scripts/index.js
@@ -11,88 +11,74 @@ function closeForm(){
 }
 addbutton.addEventListener('click', addForm);
 closebutton.addEventListener('click', closeForm);
-// const container = document.querySelector('.popup__container');
-// const form = document.querySelector('.popup__form');
-// form.addEventListener('submit', formSend);
+const container = document.querySelector('.popup__container');
+const form = document.querySelector('.popup__form');
+form.addEventListener('submit', formSend);
 
-// async function formSend(e) {
-//     e.preventDefault();
-//     let error = formValidate(form);
-//     let formData = new FormData(form);
-//     if (error === 0){
-//         container.classList.add('popup__container_sending');
-//         form.setAttribute('action', 'http://formspree.io/yule444ka1234@mail.ru');
+async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+    let formData = new FormData(form);
+    if (error === 0){
+        container.classList.add('popup__container_sending');
+        $('#gform').on('submit', function(e) {
+            $('#gform *').fadeOut(2000);
+            $('#gform').prepend('Your submission has been processed...');
+            });
+    } else {
+        alert('Заполните обязательные поля');
+    }
 
-        // for (var i=0; i < sendform.attributes.length; i++) {
-        //     if (sendform.attributes[i].nodeName == 'action') {
-        //     sendform.attributes[i].nodeValue = 'http://formspree.io/yule444ka1234@mail.ru';
-        //     break;
-        //     }
-        // }
-        // if (response.ok) {
-        //     let result = await response.json();
-        //     alert(result.message);
-        //     formPreview.innerHTML = '';
-        //     form.reset();
-        //     container.classList.remove('popup__container_sending');
-        // } else {
-        //     alert("Произошла ошибка");
-        //     container.classList.remove('popup__container_sending');
-        // }
-//     } else {
-//         alert('Заполните обязательные поля');
-//     }
+}
 
-// }
+function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('.popup__item_req');
+    for (let index = 0; index < formReq.length; index++) {
+        const input = formReq[index];
+        formRemoveError(input);
 
-// function formValidate(form) {
-//     let error = 0;
-//     let formReq = document.querySelectorAll('.popup__item_req');
-//     for (let index = 0; index < formReq.length; index++) {
-//         const input = formReq[index];
-//         formRemoveError(input);
+        if (input.classList.contains('popup__item_type_email')){
+            if (emailTest(input)){
+                formAddError(input);
+                error++;
+            }
+        }
+        if (input.classList.contains('popup__item_type_name')){
+            if (input.value === ''){
+                formAddError(input);
+                error++;
+            }
+        }
+        if (input.classList.contains('popup__item_type_telephone')){
+            if (input.value === ''){
+                formAddError(input);
+                error++;
+            }
+        }
+        if (input.classList.contains('popup__item_type_description')){
+            if (input.value === ''){
+                formAddError(input);
+                error++;
+            }
+        }
+    }
+    return error;
+}
 
-//         if (input.classList.contains('popup__item_type_email')){
-//             if (emailTest(input)){
-//                 formAddError(input);
-//                 error++;
-//             }
-//         }
-//         if (input.classList.contains('popup__item_type_name')){
-//             if (input.value === ''){
-//                 formAddError(input);
-//                 error++;
-//             }
-//         }
-//         if (input.classList.contains('popup__item_type_telephone')){
-//             if (input.value === ''){
-//                 formAddError(input);
-//                 error++;
-//             }
-//         }
-//         if (input.classList.contains('popup__item_type_description')){
-//             if (input.value === ''){
-//                 formAddError(input);
-//                 error++;
-//             }
-//         }
-//     }
-//     return error;
-// }
-
-// function formAddError(input) {
-//     // input.parentElement.classList.add('popup__item_error');
-//     input.classList.add('popup__item_error');
+function formAddError(input) {
+    // input.parentElement.classList.add('popup__item_error');
+    input.classList.add('popup__item_error');
         
     
-// }
-// function formRemoveError(input) {
-//     // input.parentElement.classList.remove('popup__item_error');
-//     input.classList.remove('popup__item_error');
+}
+function formRemoveError(input) {
+    // input.parentElement.classList.remove('popup__item_error');
+    input.classList.remove('popup__item_error');
         
-// }
+}
 
-// //email тест
-// function emailTest(input) {
-//     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-// }
+//email тест
+function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
